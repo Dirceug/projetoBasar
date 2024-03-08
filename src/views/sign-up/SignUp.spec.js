5fddcd63a632bb4a6640e3f91e4b368e27cbfac1
Dirceu Garcia <70041892+Dirceug@users.noreply.github.com>
2024-03-07 21:51:37 -0300

    (07/03) Estilos, override de estilos edisable button on multiple queries

--- src/views/sign-up/SignUp.spec.js
+++ src/views/sign-up/SignUp.spec.js
@@ -1,7 +1,12 @@
-import { render, screen } from '@testing-library/vue'
+import { render, screen, waitFor } from '@testing-library/vue'
 import '@testing-library/jest-dom'
 import SignUp from './SignUp.vue'
 import userEvent from '@testing-library/user-event'
+import { setupServer } from 'msw/node'
+import { HttpResponse, http } from 'msw'
+
+const mockFetch = vi.fn()
+global.fetch = mockFetch
 
 describe('Sign Up', () => {
   it('has Sign Up header', () => {
@@ -9,48 +14,47 @@ describe('Sign Up', () => {
     const header = screen.getByRole('heading', { name: 'Sign Up' })
     expect(header).toBeInTheDocument()
   })
-  
-  it ('has a label username input', () => {
+
+  it('has a label username input', () => {
     render(SignUp)
     expect(screen.queryByLabelText('Username')).toBeInTheDocument()
-  })  
-  it ('has username input placeholder', () => {
+  })
+  it('has username input placeholder', () => {
     render(SignUp)
     expect(screen.queryByPlaceholderText('Escolha um apelido')).toBeInTheDocument()
   })
 
-  it ('has a label e-mail input', () => {
+  it('has a label e-mail input', () => {
     render(SignUp)
     expect(screen.queryByLabelText('E-mail')).toBeInTheDocument()
   })
-  it ('has e-mail input placeholder', () => {
+  it('has e-mail input placeholder', () => {
     render(SignUp)
     expect(screen.queryByPlaceholderText('Digite aqui seu e-mail')).toBeInTheDocument()
   })
 
-  it ('has a label Password input', () => {
+  it('has a label Password input', () => {
     render(SignUp)
     expect(screen.queryByLabelText('Password')).toBeInTheDocument()
   })
-  it ('has Password input placeholder', () => {
+  it('has Password input placeholder', () => {
     render(SignUp)
     expect(screen.queryByPlaceholderText('Coloque aqui a senha')).toBeInTheDocument()
   })
-  it ('has a password type for  Password input', () => {
+  it('has a password type for  Password input', () => {
     render(SignUp)
     expect(screen.queryByLabelText('Password').type).toBe('password')
-    // expect(screen.queryByLabelText('Password')).toHaveAttribute('type', 'password')
   })
 
-  it ('has a label repeat input', () => {
+  it('has a label repeat input', () => {
     render(SignUp)
     expect(screen.queryByLabelText('Password Repeat')).toBeInTheDocument()
   })
-  it ('has Password repeat input placeholder', () => {
+  it('has Password repeat input placeholder', () => {
     render(SignUp)
     expect(screen.queryByPlaceholderText('Coloque aqui a senha novamente')).toBeInTheDocument()
   })
-  it ('has a password repeat type for Password input', () => {
+  it('has a password repeat type for Password input', () => {
     render(SignUp)
     expect(screen.queryByLabelText('Password Repeat').type).toBe('password')
     // expect(screen.queryByLabelText('Password')).toHaveAttribute('type', 'password')
@@ -60,21 +64,88 @@ describe('Sign Up', () => {
     render(SignUp)
     const button = screen.getByRole('button', { name: 'Sign Up' })
   })
-  it ('disables the button initially', () => {
+  it('disables the button initially', () => {
     render(SignUp)
-    expect(screen.getByRole('button', { name: 'Sign Up'})).toBeDisabled()
+    expect(screen.getByRole('button', { name: 'Sign Up' })).toBeDisabled()
   })
 
-  describe ('when user stes same value for password inputs', () => {
-    it('enables buttons', async() => {
+  describe('when user sets same value for password inputs', () => {
+    it('enables buttons', async () => {
       const user = userEvent.setup()
       render(SignUp)
       const passwordInput = screen.getByLabelText('Password')
       const passwordRepeatInput = screen.getByLabelText('Password Repeat')
-      await user.type(passwordInput, 'P4ssword');
+      await user.type(passwordInput, 'P4ssword')
       await user.type(passwordRepeatInput, 'P4ssword')
-      expect(screen.getByRole('button', { name: 'Sign Up'})).toBeEnabled()
+      expect(screen.getByRole('button', { name: 'Sign Up' })).toBeEnabled()
+    })
+
+    describe('when user submits form', () => {
+      it('sends username, email password to the backend', async () => {
+        let requestBody
+        const server = setupServer(
+          http.post('/api/v1/users', async ({ request }) => {
+            requestBody = await request.json()
+            return HttpResponse.json({})
+          })
+        )
+        server.listen()
+        const user = userEvent.setup()
+        render(SignUp) //Função que renderiza o componente
+        const usernameInput = screen.getByLabelText('Username')
+        const emailInput = screen.getByLabelText('E-mail')
+        const passwordInput = screen.getByLabelText('Password')
+        const passwordRepeatInput = screen.getByLabelText('Password Repeat')
+        await user.type(usernameInput, 'user1')
+        await user.type(emailInput, 'user1@mail.com')
+        await user.type(passwordInput, 'P4ssword')
+        await user.type(passwordRepeatInput, 'P4ssword')
+        const button = screen.getByRole('button', { name: 'Sign Up' })
+        await user.click(button)
+        await waitFor(() => {
+          expect(requestBody).toEqual({
+            username: 'user1',
+            email: 'user1@mail.com',
+            password: 'P4ssword'
+          })
+        })
+        server.close()
+      })
     })
   })
 
+  describe('When there is an ongoing API call', () => {
+    it.only('does not alow clicking in the button'),
+      async () => {
+        let counter = 0
+        // let requestBody
+        const server = setupServer(
+          http.post('/api/v1/users', () => {
+            // requestBody = await request.json()
+            counter += 1
+            return HttpResponse.json({})
+          })
+        )
+        server.listen()
+        const user = userEvent.setup()
+        render(SignUp) //Função que renderiza o componente
+        const usernameInput = screen.getByLabelText('Username')
+        const emailInput = screen.getByLabelText('E-mail')
+        const passwordInput = screen.getByLabelText('Password')
+        const passwordRepeatInput = screen.getByLabelText('Password Repeat')
+        await user.type(usernameInput, 'user1')
+        await user.type(emailInput, 'user1@mail.com')
+        await user.type(passwordInput, 'P4ssword')
+        await user.type(passwordRepeatInput, 'P4ssword')
+        const button = screen.getByRole('button', { name: 'Sign Up' })
+        await user.click(button)
+        await user.click(button)
+        await user.click(button)
+        await user.click(button)
+        await waitFor(() => {
+          expect(counter).toBe(1)
+        })
+        server.close()
+      }
+  })
 })
--- src/views/sign-up/SignUp.vue
+++ src/views/sign-up/SignUp.vue
@@ -1,61 +1,114 @@
+<style scoped></style>
+
 <template>
-  <div>
-    <h1>Sign Up</h1>
-    <div>
-      <label for="username">Username</label>
-      <input id="username" placeholder="Escolha um apelido" />
-    </div>
-    {{ username }}
-    <div>
-      <label for="email">E-mail</label>
-      <input id="email" placeholder="Digite aqui seu e-mail" />
-    </div>
-    <div>
-      <label for="password">Password</label>
-      <input 
-        id="password" 
-        type="password" 
-        placeholder="Coloque aqui a senha" 
-        v-model="password" 
-      />
-    </div>
-    <div>
-      <label for="passwordRepeat">Password Repeat</label>
-      <input
-        id="passwordRepeat"
-        type="password"
-        placeholder="Coloque aqui a senha novamente"
-        v-model="passwordRepeat"
-      />
-    </div>
-    <button :disabled="isDisabled">Sign Up</button>
+  <div class="col-lg-6 offset-lg-3 col-md-8 offset-md-2">
+    <form @submit.prevent="submit" class="card">
+      <div class="card-header text-center">
+        <h1>Sign Up</h1>
+      </div>
+      <div class="card-body">
+        <div class="mb-3">
+          <label class="form-label" for="username">Username</label>
+          <input
+            class="form-control"
+            id="username"
+            placeholder="Escolha um apelido"
+            v-model="formState.username"
+          />
+        </div>
+        {{ username }}
+        <div class="mb-3">
+          <label class="form-label" for="email">E-mail</label>
+          <input
+            class="form-control"
+            id="email"
+            placeholder="Digite aqui seu e-mail"
+            v-model="formState.email"
+          />
+        </div>
+        <div class="mb-3">
+          <label class="form-label" for="password">Password</label>
+          <input
+            class="form-control"
+            id="password"
+            type="password"
+            placeholder="Coloque aqui a senha"
+            v-model="formState.password"
+          />
+        </div>
+        <div class="mb-3">
+          <label class="form-label" for="passwordRepeat">Password Repeat</label>
+          <input
+            class="form-control"
+            id="passwordRepeat"
+            type="password"
+            placeholder="Coloque aqui a senha novamente"
+            v-model="formState.passwordRepeat"
+          />
+        </div>
+        <div class="text-center">
+          <button :disabled="isDisabled || apiProgress" class="btn btn-primary">Sign Up</button>
+        </div>
+      </div>
+    </form>
   </div>
 </template>
 
-<script setup>
-import { ref, computed } from 'vue'
-
-const password = ref('')
-const passwordRepeat = ref('')
-const username = ref('')
-
-const isDisabled = computed(() => {
-  return password.value || passwordRepeat.value ? password.value !== passwordRepeat.value : true
+<!-- <script setup>
+import { ref, reactive, computed } from 'vue'
+import axios from 'axios'
+const formState = reactive({
+  username: '',
+  email: '',
+  password: '',
+  passwordRepeat: ''
 })
 
-</script>
+const apiProgress = ref(false)
 
-<!-- <script>
-  export default{
-    data() {
-      return {
+const submit = () => {
+  apiProgress.value = true
+  const { passwordRepeat, ...body } = formState
+  axios.post(
+    '/api/v1/users',
+    body
+  )
+}
+
+const isDisabled = computed(() => {
+  return formState.password || formState.passwordRepeat
+    ? formState.password !== formState.passwordRepeat
+    : true
+})
+</script> -->
+
+<script>
+import axios from 'axios'
+export default {
+  data() {
+    return {
+      formState: {
+        username: '',
+        email: '',
         password: '',
-        passwordRepeat: '',
-      }
-    }, computed: {
-      isDisabled(){
-        return (this.password || this.passwordRepeat) ? this.password !== this.passwordRepeat: true 
-      }
+        passwordRepeat: ''
+      },
+      apiProgress: false
+    }
+  },
+  methods: {
+    submit() {
+      this.apiProgress = true
+      const { passwordRepeat, ...body } = this.formState
+      axios.post('api/v1/users', body)
+    }
+  },
+  computed: {
+    isDisabled() {
+      return this.formState.password || this.formState.passwordRepeat
+        ? this.formState.password !== this.formState.passwordRepeat
+        : true
     }
   }
-</script> -->
+}
+</script>
